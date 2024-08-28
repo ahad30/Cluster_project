e2cf51083b5885f9dd9a28e06d5f347c00f2a35a
Mainul7669 <mainul7669@gmail.com>
2024-08-28 13:55:03 +0600

    nodemailer email done

--- src/app/(pages)/PartnerProgram/QuickStartForm.jsx
+++ src/app/(pages)/PartnerProgram/QuickStartForm.jsx
@@ -1,102 +1,155 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
 
 const QuickStartForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    telephone: "",
+    company: "",
+    address: "",
+    template:"one"
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+
+
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const formBody = new FormData();
+    for (const [key, value] of Object.entries(formData)) {
+      formBody.append(key, value);
+    }
+
+    try {
+      const response = await fetch("/api/email", {
+        method: "post",
+        body: formBody,
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      toast.success("Message successfully sent"); // Success toast notification
+
+      // Reset form fields
+      setFormData({
+        email: "",
+        firstName: "",
+        lastName: "",
+        telephone: "",
+        company: "",
+        address: "",
+          
+      });
+    } catch (err) {
+      console.error(err);
+      toast.error("Error, please try resubmitting the form"); // Error toast notification
+    }
+  }
+
+
   return (
-    <div className='w-[90%] lg:max-w-[70%] lg:px-10 mx-auto mt-14 border-2  border-primary py-5'>
+    <div className='w-[90%] lg:max-w-[70%] lg:px-10 mx-auto mt-14 border-2 border-primary py-5'>
       <h2 className="lg:text-3xl font-semibold mb-10 text-center">Quick Start</h2>
+      <Toaster position="top-center" reverseOrder={false} />
+      <form onSubmit={handleSubmit}>
+        <div className="py-8 px-5 text-base gap-8 text-gray-700 sm:text-lg grid grid-cols-1 sm:grid-cols-2">
+          <div className="relative">
+            <label htmlFor="email">Primary Email *</label>
+            <input
+              id="email"
+              name="email"
+              type="text"
+              value={formData.email}
+              onChange={handleChange}
+              className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
+            />
+          </div>
 
-        <form>
-       <div className="">
-                <div className="py-8 px-5 text-base  gap-8 text-gray-700 sm:text-lg  grid grid-cols-1 sm:grid-cols-2">
+          <div className="relative">
+            <label htmlFor="firstName">First name *</label>
+            <input
+              id="firstName"
+              name="firstName"
+              type="text"
+              value={formData.firstName}
+              onChange={handleChange}
+              className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
+            />
+          </div>
 
-                  <div className="relative">
-                    <label htmlFor="">Primary Email *</label>
-                    <input
-                      // {...register("email")}
-                    //   defaultValue={"admin@gmail.com"}
-                    
-                      id="email"
-                      name="email"
-                      type="text"
-                      className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary focus:borer-rose-600"
-              
-                    />
-                  </div>
+          <div className="relative">
+            <label htmlFor="lastName">Last name *</label>
+            <input
+              id="lastName"
+              name="lastName"
+              type="text"
+              value={formData.lastName}
+              onChange={handleChange}
+              className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
+            />
+          </div>
 
-                  <div className="relative">
-                  <label htmlFor="">First name *</label>
-                    <input
-                      // {...register("password")}
-                    //   defaultValue={"password"}
-                      id="firstName"
-                      name="firstName"
-                      type="text"
-                      className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 
-                                    focus:outline-none focus:border-b-primary"
-                      
-                    />
-                  </div>
+          <div className="relative">
+            <label htmlFor="telephone">Telephone *</label>
+            <input
+              id="telephone"
+              name="telephone"
+              type="number"
+              value={formData.telephone}
+              onChange={handleChange}
+              className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
+            />
+          </div>
 
-                  <div className="relative">
-                  <label htmlFor="">Last name *</label>
-                    <input
-                      // {...register("password")}
-                    //   defaultValue={"password"}
-                      id="lastName"
-                      name="lastName"
-                      type="text"
-                      className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 
-                                    focus:outline-none focus:border-b-primary"
-                                          />
-                  </div>
-                  <div className="relative">
-                  <label htmlFor="">Telephone *</label>
-                    <input
-                      // {...register("password")}
-                    //   defaultValue={"password"}
-                      id="telephone"
-                      name="telephone"
-                      type="number"
-                      className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 
-                                    focus:outline-none focus:border-b-primary"
-                                        />
-                  </div>
+          <div className="relative">
+            <label htmlFor="company">Company *</label>
+            <input
+              id="company"
+              name="company"
+              type="text"
+              value={formData.company}
+              onChange={handleChange}
+              className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
+            />
+          </div>
 
-                  <div className="relative">
-                  <label htmlFor="">Company *</label>
-                    <input
-                      // {...register("password")}
-                    //   defaultValue={"password"}
-                      id="company"
-                      name="company"
-                      type="text"
-                      className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 
-                                    focus:outline-none focus:border-b-primary"
-                                        />
-                  </div>
-                  <div className="relative">
-                  <label htmlFor="">Address *</label>
-                    <input
-                      // {...register("password")}
-                    //   defaultValue={"password"}
-                      id="address"
-                      name="address"
-                      type="text"
-                      className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 
-                                    focus:outline-none focus:border-b-primary"
-                                        />
-                  </div>
-                </div>
-                  <div className="flex justify-center mb-5">
-                    <input
-                      type="submit"
-                      className="bg-primary disabled:cursor-not-allowed disabled:bg-blue-200 text-white  px-7  py-1 cursor-pointer"
-                    />
-                  </div>
-              </div>
-        </form>
+          <div className="relative">
+            <label htmlFor="address">Address *</label>
+            <input
+              id="address"
+              name="address"
+              type="text"
+              value={formData.address}
+              onChange={handleChange}
+              className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
+            />
+          </div>
+        </div>
+        
+        <div className="flex justify-center mb-5">
+          <input
+            type="submit"
+            className="bg-primary disabled:cursor-not-allowed disabled:bg-blue-200 text-white px-7 py-1 cursor-pointer"
+          />
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default QuickStartForm
+export default QuickStartForm;
--- src/app/(pages)/Support/SupportForm.jsx
+++ src/app/(pages)/Support/SupportForm.jsx
@@ -1,14 +1,76 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Icon1 from "../../../../public/Support/facebook.svg";
 import Icon2 from "../../../../public/Support/linkedin.svg";
 import Icon3 from "../../../../public/Support/youtube.svg";
 import Image from "next/image";
-
+import { toast, Toaster } from "react-hot-toast";
 const SupportForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    telephone: "",
+    message: "",
+    file: null,
+    template:"two"
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    setFormData({
+      ...formData,
+      file: e.target.files[0],
+    });
+    console.log(setFormData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Form Data:", formData);
+    const formBody = new FormData();
+    for (const [key, value] of Object.entries(formData)) {
+      formBody.append(key, value);
+    }
+
+    try {
+      const response = await fetch("/api/email", {
+        method: "post",
+        body: formBody,
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      toast.success("Message successfully sent"); // Success toast notification
+
+      // Reset form fields
+      setFormData({
+        firstName: "",
+        lastName: "",
+        telephone: "",
+        message: "",
+        file: null,
+      });
+    } catch (err) {
+      console.error(err);
+      toast.error("Error, please try resubmitting the form"); 
+    }
+  };
+
   return (
     <div className="max-w-[90%] lg:max-w-7xl lg:px-10 mx-auto mb-16">
+        <Toaster position="top-center" reverseOrder={false} />
       <div>
-        <h2 className="lg:text-2xl text-center lg:text-start font-semibold mb-2 ">
+        <h2 className="lg:text-2xl text-center lg:text-start font-semibold mb-2">
           We are in Social networks
         </h2>
         <div className="flex justify-center lg:justify-start items-center space-x-4 mt-8">
@@ -27,80 +89,85 @@ const SupportForm = () => {
         </p>
       </div>
       <div className="lg:flex lg:justify-end lg:-mt-[540px]">
-        <form className="lg:max-w-[45%] border-2 lg:border-8  border-primary lg:px-5 py-5 bg-white">
-          <div className="">
-            <h2 className="lg:text-3xl text-center lg:text-start font-semibold mb-2 ">
+        <form
+          className="lg:max-w-[45%] border-2 lg:border-8 border-primary lg:px-5 py-5 bg-white"
+          onSubmit={handleSubmit}
+        >
+          <div>
+            <h2 className="lg:text-3xl text-center lg:text-start font-semibold mb-2">
               Feedback
             </h2>
-            <div className="py-8 px-5 text-base  gap-8 text-gray-700 sm:text-lg  grid grid-cols-1 sm:grid-cols-1">
+            <div className="py-8 px-5 text-base gap-8 text-gray-700 sm:text-lg grid grid-cols-1 sm:grid-cols-1">
               <div className="relative">
-                <label htmlFor="">First name *</label>
+                <label htmlFor="firstName">First name *</label>
                 <input
-                  // {...register("password")}
-                  //   defaultValue={"password"}
                   id="firstName"
                   name="firstName"
                   type="text"
-                  className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 
-                                  focus:outline-none focus:border-b-primary"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
                 />
               </div>
 
               <div className="relative">
-                <label htmlFor="">Last name *</label>
+                <label htmlFor="lastName">Last name *</label>
                 <input
-                  // {...register("password")}
-                  //   defaultValue={"password"}
                   id="lastName"
                   name="lastName"
                   type="text"
-                  className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 
-                                  focus:outline-none focus:border-b-primary"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
                 />
               </div>
               <div className="relative">
-                <label htmlFor="">Telephone *</label>
+                <label htmlFor="telephone">Telephone *</label>
                 <input
-                  // {...register("password")}
-                  //   defaultValue={"password"}
                   id="telephone"
                   name="telephone"
                   type="number"
-                  className="peer  h-10 w-full border-b-2 border-gray-300 text-gray-900 
-                                  focus:outline-none focus:border-b-primary"
+                  value={formData.telephone}
+                  onChange={handleChange}
+                  className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-b-primary"
                 />
               </div>
 
               <div className="relative">
                 <textarea
-                  // {...register("password")}
-                  //   defaultValue={"password"}
-                  id="company"
-                  name="company"
-                  type="text"
-                  className="peer  h-24 w-full border-2 border-gray-300 text-gray-900 
-                                  focus:outline-none focus:border-primary p-2"
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  className="peer h-24 w-full border-2 border-gray-300 text-gray-900 focus:outline-none focus:border-primary p-2"
                   placeholder="Message *"
                 ></textarea>
               </div>
 
-              <div class="relative ">
-                <label htmlFor="">Add file and press "Upload"</label>
+              <div className="relative">
+                <label htmlFor="file">
+                  Add file and press &apos;Upload&apos;
+                </label>
                 <div className="lg:border lg:border-gray-600 lg:border-r-0 flex items-center mt-3">
-                  <input type="file" className="ms-1" />
-                  <button
-                    class="bg-green-500 text-white px-4 py-2 hover:bg-blue-600
-                 text-[8px] lg:text-sm lg:h-[45px] hidden lg:block -ml-7"
+                  <input
+                    id="file"
+                    name="file"
+                    type="file"
+                    onChange={handleFileChange}
+                    className="ms-1"
+                  />
+                  {/* <button
+                    className="bg-green-500 text-white px-4 py-2 hover:bg-blue-600 text-[8px] lg:text-sm lg:h-[45px] hidden lg:block -ml-7"
                   >
                     Upload
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </div>
             <div className="flex justify-center mb-5 px-5">
               <input
                 type="submit"
-                className="bg-primary disabled:cursor-not-allowed disabled:bg-blue-200 text-white  py-1 cursor-pointer w-full"
+                className="bg-primary disabled:cursor-not-allowed disabled:bg-blue-200 text-white py-1 cursor-pointer w-full"
               />
             </div>
           </div>
