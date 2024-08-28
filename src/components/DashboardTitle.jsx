f8b5e6ce3d0605892f5c6a05571bc76bf2d78572
ahad30 <mohiminulislamahad@gmail.com>
2024-08-28 20:31:15 +0600

    stripe implemented completed

--- src/components/DashboardTitle.jsx
+++ src/components/DashboardTitle.jsx
@@ -1,5 +1,4 @@
 "use client"
-import { Card } from "@material-tailwind/react";
 import { useEffect } from "react";
 
 const DashboardTitle = ({ text, children, windowTitle }) => {
@@ -10,11 +9,12 @@ const DashboardTitle = ({ text, children, windowTitle }) => {
   }, [windowTitle]);
 
   return (
-    <Card className="p-3">
-      <h2 className="text-xl font-Poppins">
+     <div>
+     <h2 className="text-xl font-Poppins">
         {text} {children}
       </h2>
-    </Card>
+     </div>
+
   );
 };
 
