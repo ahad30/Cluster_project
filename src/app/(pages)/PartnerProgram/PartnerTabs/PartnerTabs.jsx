de58f421dfd5df136b5d6f1936994a48a149185e
ahad30 <mohiminulislamahad@gmail.com>
2024-09-17 16:46:38 +0600

    dsd

--- src/app/(pages)/PartnerProgram/PartnerTabs/PartnerTabs.jsx
+++ src/app/(pages)/PartnerProgram/PartnerTabs/PartnerTabs.jsx
@@ -13,8 +13,8 @@ const PartnerTabs = () => {
         <div className='lg:flex gap-5'>
           <div className='lg:border-r-2 lg:border-r-blue-gray-600 lg:h-[550px] lg:me-5'>
             <TabList className='flex flex-col gap-y-2 lg:me-4 text-center lg:text-start list-none'>
-              <Tab className='py-2 px-4 border-2 border-gray-300  cursor-pointer outline-none lg:w-[250px]' selectedClassName='bg-green-500 text-white '>Cluster! Distributor</Tab>
-              <Tab className='py-2 px-4 border-2 border-gray-300 cursor-pointer outline-none' selectedClassName='bg-green-500 text-white'>Cluster! Reseller</Tab>
+              <Tab className='py-2 px-4 border-2 border-gray-300  cursor-pointer outline-none lg:w-[250px]' selectedClassName='bg-green-500 text-white '>Cluster Distributor</Tab>
+              <Tab className='py-2 px-4 border-2 border-gray-300 cursor-pointer outline-none' selectedClassName='bg-green-500 text-white'>Cluster Reseller</Tab>
               <Tab className='py-2 px-4 border-2 border-gray-300 cursor-pointer outline-none' selectedClassName='bg-green-500 text-white'>Cluster affiliate</Tab>
             </TabList>
           </div>
